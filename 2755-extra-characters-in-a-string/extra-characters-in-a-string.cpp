c402357390db10d5864483db939561cd0bfd499d
Nakul Kumar <149577160+Nakul-Kumar01@users.noreply.github.com>
2026-08-25 00:39:14 +0530

    Time: 63 ms (63.66%) | Memory: 69.8 MB (80.18%) - LeetSync

--- 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
@@ -1,35 +1,55 @@
 class Solution {
 public:
+    int find(int i, string& s, unordered_set<string>& m, vector<int>& dp) {
+        if (i >= s.size())
+            return 0;
 
-    int find(int i,string &s,unordered_set<string>&m,vector<int>&dp){
-        if(i>=s.size()) return 0;
+        if (dp[i] != -1)
+            return dp[i];
 
-        if(dp[i] != -1) return dp[i];
-        
         int res = INT_MAX;
 
-        res = min(res,1 + find(i+1,s,m,dp));
+        res = min(res, 1 + find(i + 1, s, m, dp));
 
         string t = "";
-        for(int j=i;j<s.size();j++){
-           t += s[j];
-           if(m.count(t)){
-            res = min(res,find(j+1,s,m,dp));
-           }
+        for (int j = i; j < s.size(); j++) {
+            t += s[j];
+            if (m.count(t)) {
+                res = min(res, find(j + 1, s, m, dp));
+            }
         }
 
         return dp[i] = res;
     }
 
     int minExtraChar(string s, vector<string>& dic) {
-        
+
         int n = s.size();
 
-        unordered_set<string>m;
-        for(auto x: dic) m.insert(x);
-        
-        vector<int>dp(n+1,-1);
+        unordered_set<string> m;
+        for (auto x : dic)
+            m.insert(x);
 
-        return find(0,s,m,dp);
+        vector<int> dp(n + 1, 0);
+
+        // return find(0,s,m,dp);
+
+        for (int i = n - 1; i >= 0; --i) {
+            int res = INT_MAX;
+
+            res = min(res, 1 + dp[i+1]);
+
+            string t = "";
+            for (int j = i; j < s.size(); j++) {
+                t += s[j];
+                if (m.count(t)) {
+                    res = min(res, dp[j+1]);
+                }
+            }
+
+             dp[i] = res;
+        }
+
+        return dp[0];
     }
 };
