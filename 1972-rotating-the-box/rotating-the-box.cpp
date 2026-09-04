773c080cb804c8a073cd17ecca9585e4646dbfc4
Nakul Kumar <149577160+Nakul-Kumar01@users.noreply.github.com>
2026-09-04 12:05:35 +0530

    Time: 1996 ms (5.06%) | Memory: 56.7 MB (90.43%) - LeetSync

---
+++ 1972-rotating-the-box/rotating-the-box.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& bg) {
+
+        int n = bg.size(), m = bg[0].size();
+
+        // apply gravity firsst
+        // then rotate
+        
+        vector<vector<char>>ans(m,vector<char>(n,'.'));
+
+        for(int i=n-1;i>=0;--i){
+            int pos = m-1;
+
+            for(int j=m-1;j>=0;--j){
+                if(bg[i][j] == '#'){
+                    swap(bg[i][pos], bg[i][j]);
+                    pos = pos-1;
+                }
+                else if(bg[i][j] == '*') pos = j-1;
+            }
+        }
+        for(int i=0;i<n;i++)
+            for(int j =0;j<m;j++) cout<<bg[i][j]<<" ";
+
+
+        for(int i=0;i<n;i++){
+            for(int j =0;j<m;j++){
+               ans[j][n-i-1] = bg[i][j];
+            }
+        }
+        return ans;
+
+    }
+};
